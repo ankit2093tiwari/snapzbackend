0de3ef787b9abf02884a5676b08d38c5ce9d41fd
ankit2093tiwari <39435198+ankit2093tiwari@users.noreply.github.com>
2024-07-18 18:06:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -915,13 +915,14 @@ app.get(
           SELECT c.id FROM mdl8m_question_categories c
           JOIN category_hierarchy h ON c.parent = h.id
       )
-      SELECT q.id AS question_id, q.name AS questionName, q.questiontext AS question_text, q.qtype AS question_type,
+      SELECT q.id AS question_id, q.name AS questionName, q.questiontext AS question_text, , ti.tagid, q.qtype AS question_type,
       a.id AS answer_id, a.answer, a.fraction
       FROM mdl8m_question_bank_entries mqbe
       INNER JOIN mdl8m_question_versions mqv ON mqbe.id = mqv.questionbankentryid
       INNER JOIN mdl8m_question q ON q.id = mqv.questionid
       INNER JOIN mdl8m_question_categories mqc ON mqc.id = mqbe.questioncategoryid
       LEFT JOIN mdl8m_question_answers a ON a.question = q.id
+      JOIN mdl8m_tag_instance ti ON ti.itemid = q.id  
       WHERE mqc.id IN (SELECT id FROM category_hierarchy) AND q.qtype = 'multichoice'`;
     } else if (name.startsWith("article")) {
       query = `
